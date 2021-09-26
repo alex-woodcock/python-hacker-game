867bf99ff94720bbc20e0057aad75a0600c431b0
alex-woodcock <alexanderpwoodcock@gmail.com>
2021-09-26 18:11:21 +0100

    fixed the stuff that didnt work
    and broke some of the stuff that did

--- game.py
+++ game.py
@@ -1,15 +1,16 @@
 import random
 
-print("guess a random number between 1 and 2147483647! Its a different number every time! good luck!
-
-def game():
-    userInput = input('\nInput a math expression for your guess: ')
-    if eval(userInput + "== random.randrange(2147483647)"):
-        print("you win !")
-    else:
-        print("you lose loser")
+print("guess a random number between 1 and 2147483647! Its a different number every time! good luck!")
+print("itd take a 5up3r l33t h4ck3r to crack this code, and im not even sure you were banned from the internet when you were 11")
 
 while True:
-    game()
-    if (input("play again? y/n: ").lower()[0] == 'n'):
+    userInput = input('\nInput a math expression for your guess: ')
+    solution = random.randrange(2147483647)
+    if eval(userInput + "== random.randrange("+str(solution)+")"):
+        print("what?? how did you do that??? you win!?!? my evil scheeeeeeeemeeeesssss!!!!")
         break
+    else:
+        print("hah, nice try but you were way off, you guessed "+str(eval(userInput))+" and the answer was "+str(solution))
+
+##i was away for the start of the event but
+##it looked like fun so i thought id make something
